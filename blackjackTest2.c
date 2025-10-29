3131c40c9d99ef0e7b8fa82fa9f6f6523755eb86
CHATHAMR <CHATHAMR@my.erau.edu>
2025-10-29 16:11:02 -0700

    Updated/Compiled Blackjack .c
    Finished error checking/debugging the .c, functions perfectly as a 1 hand singleplayer program.
    TBA: Add ace functionality, add split function, continue work on file.io, refactor code.

--- blackjackTest2.c
+++ blackjackTest2.c
@@ -2,43 +2,95 @@
 #include <math.h>
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
+
+//RESOURCES: FINGOLIAN FEMBOY FOR 2D ARRAY, I CAN'T STOP WINNING
+
+/*printf("
+....................................................................................................
+....................................................................................................
+....................................................................................................
+....................................................................................................
+.....................................................@@@@@@@@@@@@@@@@@@@@@..........................
+.......@@@@@@@@@@@@@@@@.........................@@@@@......................@@@@@@@@@@@@.............
+......@@..............@@@@@@................@@@........................................@@...........
+......@@...................@@@@@........@@@@@@@@........@@@@@@@@@@..................@@@@@...........
+.......@@.......................@@@.....@.......@@@@@@@@..........@@@@@@@@.....@@@@@....@...........
+........@@@@@.....................@@...@...................................@@@..........@...........
+......@@....@@@@@@@@@@@............@..@@....................................@...........@...........
+.......@...........@@@.@@@@@@@@@@@@...@@...@@@@@...@@@@@@@..@@@@@@@@@@@@....@..........@@...........
+.......@@@@.....@@@.@@@@@...........@@@...@...@...@@..@....@@.......@..@....@..........@............
+..........@@@@@@@....@..@@@@@....@@@@.@..@@.@....@@..@..@..@@.@@..@@...@....@..........@............
+.....................@@......@@@@@....@..@..@..@.@..@@..@.@@.@@.@.@.@.@@....@..........@............
+.....................@@..............@..@@.@@.@.@@.@@..@@.@....@..@.@.@@...@@..........@............
+.....................@@..............@..@.@......@.@...@@.@.@.@...@.@.@....@...........@............
+.....................@@..............@..@@.@...@.@.@..@.@.@.@.@.@.@@..@....@..........@@............
+.....................@@..............@..@@.@..@@.@..@.@..@.@..@@@..@.@@....@...........@............
+.....................@@.............@...@..@...@.@@.@.@..@.@...@@..@..@....@...........@............
+.................@@@@@@.............@...@@..@....@@..@.@...@.@..@@.@@.@...@@...........@............
+................@@...@@@............@....@@.@..@@@.@..@.@@.@..@.@.@.@.@...@...........@@............
+................@@.....@@@.........@@.....@..@....@....@.@..@.@.......@@..@............@............
+................@@@@@@@..@@........@@......@@......@........@@@@@@@@@@@...@...........@@............
+...........@@@@@.@.....@@.@@.......@.........@@@@@@@@@@@@@.................@.........@@.............
+...........@.@..@@@@@@@@@@@@@......@.......................................@......@@@...............
+...........@..@@........@.@@@@@@@@@@............................@@.........@.@@@@@..................
+..........@@@@@@@@@@@@@@..@@........@@@@@@@@@@@@@@@@@@@@@@@@@@@@..@@@@@@@@@@@.......................
+..........@...............@.........................................................................
+..........@@@@@@@@@@@@@@@@@.........................................................................
+....................................................................................................
+  ___    ____    _    _   _ _ _____   ____ _____ ___  ____   __        _____ _   _ _   _ ___ _   _  ____ 
+ |_ _|  / ___|  / \  | \ | ( )_   _| / ___|_   _/ _ \|  _ \  \ \      / /_ _| \ | | \ | |_ _| \ | |/ ___|
+  | |  | |     / _ \ |  \| |/  | |   \___ \ | || | | | |_) |  \ \ /\ / / | ||  \| |  \| || ||  \| | |  _ 
+  | |  | |___ / ___ \| |\  |   | |    ___) || || |_| |  __/    \ V  V /  | || |\  | |\  || || |\  | |_| |
+ |___|  \____/_/   \_\_| \_|   |_|   |____/ |_| \___/|_|        \_/\_/  |___|_| \_|_| \_|___|_| \_|\____|
+");*/                                                                                                         
+
+
 
 //CARD STRUCT
 typedef struct {
   int value;
-  char name[];
+  char name[20];
   int drawn;
 } Card;
-
 
 //GENERATE DECK IN SEPARATE .TXT FILE, IMPORT DECK, SHUFFLE FOR ACTUAL PROGRAM
 
 //RANDOMLY GENERATING CARD DECK
 //TBA: ENSURE NO CARD REPEATED MORE THAN 4 TIMES PER DECK USED
-void deckShuffle(char *cardList, Card *deck)
+void deckShuffle(const char cardList[][16], Card *deck)
 {
   int i;
   srand(time(0)); //INCLUDE IN MAIN?
   
+  //printf("deckShuffle check 1");
+  
   for (i = 0; i < 104; i++)
   {
     //SETTING CARD VALUE FROM 2 TO 11
-    deck[i].value = (rand() % 11 - 2 + 1) + 2;
+    deck[i].value = (rand() % 10) + 2;
+    
+    //printf("deckShuffle check 2");
     
     //SETTING CARD NAME
     //FOR CARD VALUE 10, PICKS RANDOM NAME FROM "ten" TO "King" 
     if (deck[i].value == 10)
     {
-      deck[i].name = cardList[(rand() % (11 - 8 + 1)) + 8];
+      //deck[i].name = cardList[(rand() % (11 - 8 + 1)) + 8];
+      strcpy(deck[i].name, cardList[(rand() % 3) + 9]);
     }
     //FOR CARD VALUE 11, SET NAME TO "Ace"
     else if (deck[i].value == 11)
     {
-      deck[i].name = cardList[12];
+      //deck[i].name = cardList[12];
+      strcpy(deck[i].name, cardList[12]);
     }
     //FOR CARD VALUE UNDER 10, SET NAME TO CARD VALUE
     else
-      deck[i].name = cardList[deck[i].value - 2];
+    {
+      //deck[i].name = cardList[deck[i].value - 2];
+      strcpy(deck[i].name, cardList[deck[i].value - 2]);
+    }
     
     //ENSURING DRAWN VALUE SET TO 0
     deck[i].drawn = 0;
@@ -67,20 +119,24 @@ int input_verification_type_int(char *err_msg)
 int main()
 {
   //ARRAY OF STRINGS FOR CARD NAMES
-  cardlist[12] = {"two", "three", "four", "five", "six", "seven", "eight", 
-                  "nine", "ten", "Jack", "Queen", "King", "Ace"};
+  //const char *cardList[13] = {"two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "Jack", "Queen", "King", "Ace"};
+  const char cardList[][16] = {"two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "Jack", "Queen", "King", "Ace"};
   //ARRAY OF STRUCTS FOR CARDS
   Card deck[103];
   Card playerHand[10];
   Card dealerHand[10];
-  //PLAYER CHOICE OPTION VAR
-  int choice, playerHandValue, dealerHandValue, playerIndexPos, dealerIndexPos;
+  //INT DECLARATIONS  
+  int choice, playerHandValue, dealerHandValue, playerIndexPos, dealerIndexPos, exitFlag, i;
   
   srand(time(0));
   
-  //EXECUTING SHUFFLE FUNCTION
-  deckShuffle(&cardlist, &deck);
+  //printf("Break 1\n");
+  //printf("%ld\n", __STDC_VERSION__);
   
+  //EXECUTING SHUFFLE FUNCTION
+  deckShuffle(cardList, deck);
+  
+  //printf("Break 2\n");
   //POPULATING PLAYER & DEALER HANDS
   //TBA: ADD FUNCTIONALITY FOR CHECKING DRAWN STATUS
   for (i = 0; i < 2; i++)
@@ -89,35 +145,38 @@ int main()
     dealerHand[i] = deck[(rand() % 104)];
   }
   
+  //ace(Card *playerHand)
+
+  
   playerIndexPos = 2;
   dealerIndexPos = 2;
   
   //PRINT PLAYER HAND TO TERMINAL
-  printf("\t\t\t\t\tCard 1 name & value: %s\t%d\n\
-          Card 2 name & value: %s\t%d\n",
-          playerHand[0].name,
-          playerHand[0].value,
-          playerHand[1].name,
-          playerHand[1].value);
+  printf("\tCard 1 name & value: %s %d\n\
+        Card 2 name & value: %s %d\n",
+        playerHand[0].name,
+        playerHand[0].value,
+        playerHand[1].name,
+        playerHand[1].value);
   
   //BLACKJACK WIN CASE
   if (playerHand[0].value + playerHand[1].value == 21)
   {
-    printf("YOU ARE A WINNER\n");
+    printf("I CAN'T STOP WINNING\n");
+    return 0;
   }
   else if (dealerHand[0].value + playerHand[1].value == 21)
   {
-    printf("YOU ARE A LOSER\n");
+    printf("AW DANGIT\n");
+    return 0;
   }
   
   //PLAYER CHOICE MENU LOOP
   while (exitFlag != 1)
   {
-    //PLAYER SELECTION PRINT
-    printf("Please select from the following options: Choose (1) to hit, (2) to stand, (3) to split\n");
-    choice = input_verification_type_int("Invalid Selection: Choose (1) to hit, (2) to stand, (3) to split\n");
-    
     //PLAYER HAND VALUE CALCULATION
+    //MAKE WHILE LOOP EXIT IF PLAYERHANDVALUE > 21
+    playerHandValue = 0;
     for (i = 0; i < 10; i++)
     {
       playerHandValue += playerHand[i].value;
@@ -126,19 +185,31 @@ int main()
       {
         printf("You busted\n");
         exitFlag = 1;
-        break
+        break;
       }
     }
     
     //PLAYER HAND VALUE PRINT STATEMENT
-    printf("Player hand value is: %d\n", playerHandValue);
+    printf("\tPlayer hand value is: %d\n", playerHandValue);
+    
+    //FIXME
+    if (exitFlag == 1)
+      break;
+    
+    //PLAYER SELECTION PRINT
+    printf("Please select from the following options: Choose (1) to hit, (2) to stand, (3) to split\n");
+    choice = input_verification_type_int("Invalid Selection: Choose (1) to hit, (2) to stand, (3) to split\n");
   
     switch (choice)
     {
       //HIT CASE
       case 1:
-        // check to see if playerhandvalue < 21 ! 
         playerHand[playerIndexPos] = deck[(rand() % 104)];
+        //ace(Card *playerHand)
+        printf("\tCard %d name & value: %s %d\n",
+              playerIndexPos,
+              playerHand[playerIndexPos].name,
+              playerHand[playerIndexPos].value);
         playerIndexPos++;
         break;
       //STAND CASE
@@ -150,42 +221,45 @@ int main()
         //playerHand1[0] = playerHand[0];
         //playerHand2[0] = playerHand[1];
         break;
-      default;
+      default:
         printf("Invalid Selection: Choose (1) to hit, (2) to stand, (3) to split\n");
         break;
     }
   }
   
-  dealerHandValue = dealerHand[0].value + dealerHand[1].value; 
+  dealerHandValue = dealerHand[0].value + dealerHand[1].value;
+  printf("Dealer Hand Value: %d\n", dealerHandValue); 
   
   //DEALER BEHAVIOR
+  //TBA: INSERT DEALER HIT INTO ELSE IF IN FOR LOOP
   while (dealerHandValue < 17)
-  {
-    dealerHand[dealerIndexPos] = deck[(rand() % 104)];
-    dealerIndexPos++;
-    
+  {    
     for (i = 0; i < 10; i++)
     {
       dealerHandValue += dealerHand[i].value;
+      printf("Dealer Hand Value: %d\n", dealerHandValue);
       
-      if (dealerHandValue > 21)
+      if (dealerHandValue > 21 && playerHandValue < 21)
       {
-        printf("YOUR WINNER\n");
-        break
+        printf("I CAN'T STOP WINNING\n");
+        return 0;
+        break;
       }
+    dealerHand[dealerIndexPos] = deck[(rand() % 104)];
+    dealerIndexPos++;
     }
   }
   
   //WIN CONDITIONS
-  if (playerHandValue > dealerHandValue)
+  if (playerHandValue > dealerHandValue && playerHandValue < 21)
   {
-    printf("YOUR WINNER\n");
+    printf("I CAN'T STOP WINNING\n");
   }
-  else if (playerHandValue < dealerHandValue)
+  else if (playerHandValue < dealerHandValue && dealerHandValue < 21)
   {
-    printf("YOUR LOOSER\n");
+    printf("AW DANGIT\n");
   }
-  else if (playerHandValue == dealerHandValue)
+  else if (playerHandValue == dealerHandValue && playerHandValue < 21)
   {
     printf("It's a tie");
   }
@@ -195,81 +269,7 @@ int main()
   return 0;
 }
   
-// WIP DEALING FUNCTION 
-int Ace(Card *playerHand)
-{ 
-  int ace;
-  for (i = 0; i < 10; i++)
-  {
-    if (playerHand[i] == 11)
-    {
-      printf("You pulled an Ace! Would you like to keep it as a 1, or change it to an 11? \n");
-      //scanf("%d", &ace);
-      // insert error checking here 
-      ace = input_validation_check_int("Invalid input. Please input 1 or 11.\n");
-      
-      if (ace == 11)
-      {
-        playerHand[i].value += 11;
-      }
-
-      else if (ace == 1)
-      {
-        playerHand[i].value += 1;
-      }
-      else
-      {
-        // insert shit be borke
-        printf("Shit be broke\n");
-      }
-  }
-}
-
   
-int Player_Hit()
-{ 
-  while playerhand[i].value < 21
-    {
-      playerHand[playerIndexPos] = deck[(rand() % 104)];
-      int Ace();
-        playerIndexPos++;
-    }
-}
-
-// idea: make a function for Dealer hits depending on CPU difficulty that the player chose 
-
-int Dealer_Hit()
-{
-   while (dealerHandValue < 17)
-  {
-    dealerHand[dealerIndexPos] = deck[(rand() % 104)];
-    dealerIndexPos++;
-    
-    for (i = 0; i < 10; i++)
-    {
-      dealerHandValue += dealerHand[i].value;
-      
-      if (dealerHandValue > 21)
-      {
-        printf("YOUR WINNER\n");
-        break
-      }
-    }
-  }
-  // can change function depending on how we want to do the dealer difficulty and such. 
-}
-
-/* int funds()
-{
-    gambling/funding functions for later if wanted
-    for ( i = 0; i < player_num; i++)
-    {
-    printf("Player %d, would you like to add funds?", i) ;
-    // something something scanf
-    // if 1, scanf for funds amount 
-    // if 2, check if they're in debt
-    // if they're in debt for a certain amount, kick them out lol 
-    
-    }
-}
-*/
+          
+          
+  
